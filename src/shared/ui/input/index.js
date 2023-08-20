ddd95efe62cd21f1575ec96cadb2de3875b749e9
Admin <admin.push@hackerrank.workspace>
2023-08-20 11:31:39 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/shared/ui/input/index.js
+++ src/shared/ui/input/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Input({ value, onChange, placeholder, rest }) {
+function Input({ value, onChange, placeholder, ...rest }) {
   return (
     <input 
       type="text" 
