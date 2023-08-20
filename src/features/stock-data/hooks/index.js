55d4378e93329c60ea6a7c196455ecbc6ca07dcd
Admin <admin.push@hackerrank.workspace>
2023-08-20 11:48:47 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/features/stock-data/hooks/index.js
+++ src/features/stock-data/hooks/index.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { fetchStockData } from '../api/fetchStock';
+import { fetchStockData } from '../api';
 
 export const useStockData = () => {
   const [inputDate, setInputDate] = useState('');
