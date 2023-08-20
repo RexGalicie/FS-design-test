655f05c75ff887630128562ca83075d3f300c26b
Admin <admin.push@hackerrank.workspace>
2023-08-20 11:07:51 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/components/stock-data/index.js
+++ src/components/stock-data/index.js
@@ -2,16 +2,60 @@ import React from "react";
 import "./index.css";
 
 export default function StockData() {
+  const [inputDate, setInputDate] = useState('');  // to store input value
+  const [stockInfo, setStockInfo] = useState(null);  // to store fetched stock data
+
+  const handleInputChange = (e) => {
+    setInputDate(e.target.value);
+  };
+
+  const fetchStockData = async () => {
+    try {
+      const response = await fetch(`https://jsonmock.hackerrank.com/api/stocks?date=${inputDate}`);
+      const data = await response.json();
+      if (data && data.data && data.data.length > 0) {
+        setStockInfo(data.data[0]);
+      } else {
+        setStockInfo(null);
+      }
+    } catch (error) {
+      console.error("Error fetching stock data:", error);
+      setStockInfo(null);
+    }
+  };
   return (
     <div className="layout-column align-items-center mt-50">
       <section className="layout-row align-items-center justify-content-center">
-        <input type="text" className="large" placeholder="5-January-2000" id="app-input" data-testid="app-input"/>
-        <button className="" id="submit-button" data-testid="submit-button">Search</button>
+      <input 
+          type="text" 
+          className="large" 
+          placeholder="5-January-2000" 
+          id="app-input" 
+          data-testid="app-input"
+          value={inputDate}
+          onChange={handleInputChange}
+        />
+        <button 
+          className="" 
+          id="submit-button" 
+          data-testid="submit-button"
+          onClick={fetchStockData}
+        >
+          Search
+        </button>
       </section>
-      <ul className="mt-50 slide-up-fade-in styled" id="stockData" data-testid="stock-data">
-        <li className="py-10"></li>
-      </ul>
-      <div className="mt-50 slide-up-fade-in" id="no-result" data-testid="no-result"></div>
+      {stockInfo ? (
+        <ul className="mt-50 slide-up-fade-in styled" id="stockData" data-testid="stock-data">
+          <li className="py-10">Open: {stockInfo.open}</li>
+          <li className="py-10">Close: {stockInfo.close}</li>
+          <li className="py-10">High: {stockInfo.high}</li>
+          <li className="py-10">Low: {stockInfo.low}</li>
+        </ul>
+      ) : inputDate ? (
+        <div className="mt-50 slide-up-fade-in" id="no-result" data-testid="no-result">
+          No Results Found
+        </div>
+      ) : null}
     </div>
   );
 }
